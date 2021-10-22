ed96543245a2ca5a19612f4614874248511c8f8c
Nicolás Ibarra <nicoibarra@nicolass-mbp.lan>
2021-10-22 14:48:13 +0200

    s sur routes

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
+mongoose.connect(process.env.MONGODB_URI);
+
 const app = express();
 app.use(formidable());
 app.use(cors());
 
 // const User = require("../Vinted/models/User");
 // const Offer = require("../Vinted/models/Offer");
-const OfferRoutes = require("./routes/OfferRoutes.js");
-const UserRoutes = require("./routes/UserRoute.js");
+const OfferRoutes = require("./routes/OfferRoutes");
+const UserRoutes = require("./routes/UserRoutes");
 
 app.use(UserRoutes);
 app.use(OfferRoutes);
-
-mongoose.connect(process.env.MONGODB_URI);
 
 app.listen(process.env.PORT, () => {
   console.log("Server has started");
